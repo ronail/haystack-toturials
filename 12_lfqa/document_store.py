cc6e7a45f2a211fabd6d244d0293f65ca09ecab9
Ronald SHOPLINE <ronailhk@gmail.com>
2023-04-01 23:15:12 +0800

    Uses OpenAI retriever for chinese compatibility

--- 12_lfqa/document_store.py
+++ 12_lfqa/document_store.py
@@ -4,4 +4,4 @@ import os.path
 if os.path.exists("index.faiss"):
   document_store = FAISSDocumentStore(faiss_index_path="index.faiss")
 else:
-  document_store = FAISSDocumentStore(embedding_dim=128, faiss_index_factory_str="Flat")
+  document_store = FAISSDocumentStore(embedding_dim=1024, faiss_index_factory_str="Flat")
--- 12_lfqa/retriever.py
+++ 12_lfqa/retriever.py
@@ -3,10 +3,20 @@ from document_store import document_store
 
 from haystack.document_stores import FAISSDocumentStore
 
-retriever = DensePassageRetriever(
-    document_store=document_store,
-    query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki",
-    passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",
+# retriever = DensePassageRetriever(
+#     document_store=document_store.document_store,
+#     query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki",
+#     passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",
+# )
+
+from haystack.nodes import EmbeddingRetriever
+import os
+retriever = EmbeddingRetriever(
+   document_store=document_store,
+   batch_size=8,
+   embedding_model="ada",
+   api_key=os.environ.get('OPENAI_KEY'),
+   max_seq_len=1024
 )
 
 
